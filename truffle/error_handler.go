2253ce0152a0aca65fb038120ce66fb8df0aa3bf
LAPTOP-3E4II3DG\maotan <mt_126@126.com>
2021-01-10 10:34:49 +0800

    error handler changes

--- truffle/error_handler.go
+++ truffle/error_handler.go
@@ -6,7 +6,6 @@
 package truffle
 
 import (
-	"fmt"
 	"github.com/gin-gonic/gin"
 	log "github.com/sirupsen/logrus"
 	"net/http"
@@ -17,12 +16,8 @@ func Recover(c *gin.Context) {
 	defer func() {
 		if r := recover(); r != nil {
 			//打印错误堆栈信息
-			msg := fmt.Sprintf("panic: %v\n", r)
-			log.Info(msg)
-			debug.PrintStack()
-			//封装通用json返回
-			//c.JSON(http.StatusOK, Result.Fail(errorToString(r)))
-			//Result.Fail不是本例的重点，因此用下面代码代替
+			log.Infof("panic: %#v", r)
+			//debug.PrintStack()
 			switch v := r.(type) {
 			case WarnError:
 				c.JSON(http.StatusOK, gin.H{
@@ -32,12 +27,14 @@ func Recover(c *gin.Context) {
 				})
 			case error:
 				c.JSON(http.StatusOK, Fail(-1, v.Error()))
+				debug.PrintStack()
 			default:
 				c.JSON(http.StatusOK, gin.H{
 					"code": -1,
 					"msg":  r.(string),
 					"data": nil,
 				})
+				debug.PrintStack()
 			}
 
 			//终止后续接口调用，不加的话recover到异常后，还会继续执行接口里后续代码
